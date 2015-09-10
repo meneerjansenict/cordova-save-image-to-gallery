92cbef1a25378a662a124beea06f62de67862deb
Simba Zhang <solderzzc@gmail.com>
2015-09-10 12:12:56 -0700

    Update Canvas2ImagePlugin.js

--- www/Canvas2ImagePlugin.js
+++ www/Canvas2ImagePlugin.js
@@ -1,15 +1,16 @@
 //
 //  Canvas2ImagePlugin.js
-//  Canvas2ImagePlugin PhoneGap/Cordova plugin
+//  Base64ImageSaverPlugin PhoneGap/Cordova plugin
 //
 //  Created by Tommy-Carlos Williams on 29/03/12.
+//  Modified by Simba Zhang on 09/10/2015
 //  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
 //  MIT Licensed
 //
 
   module.exports = {
     
-    saveImageDataToLibrary:function(successCallback, failureCallback, canvasId) {
+    saveImageDataToLibrary:function(successCallback, failureCallback, base64Data) {
         // successCallback required
         if (typeof successCallback != "function") {
             console.log("Canvas2ImagePlugin Error: successCallback is not a function");
@@ -18,8 +19,7 @@
             console.log("Canvas2ImagePlugin Error: failureCallback is not a function");
         }
         else {
-            var canvas = (typeof canvasId === "string") ? document.getElementById(canvasId) : canvasId;
-            var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
+            var imageData = base64Data.replace(/data:image\/png;base64,/,'');
             return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData]);
         }
     }
