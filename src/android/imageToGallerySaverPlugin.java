67c77bfbb2de8581d3310ef4d7d670f10467a52b
Mathieu Kooiman <mathieu.kooiman@flexkids.nl>
2018-08-22 11:16:14 +0200

    Update functionality to work in recent versions

--- src/android/imageToGallerySaverPlugin.java
+++ src/android/imageToGallerySaverPlugin.java
@@ -10,7 +10,9 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -28,45 +30,81 @@ import android.util.Log;
  *
  * @author Vegard Løkken <vegard@headspin.no>
  */
-public class imageToGallerySaverPlugin extends CordovaPlugin {
-	public static final String ACTION = "saveImageDataToLibrary";
+public class Base64ImageSaverPlugin extends CordovaPlugin {
+	private static final String ACTION = "saveImageDataToLibrary";
+
+	// Permissions
+	private static final String WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+	private static final int STORE_PHOTO_REQ_CODE = 0;
+
+	// Utility
+	private CallbackContext callbackContext;
+	private Bitmap bitmapImage;
 
 	@Override
 	public boolean execute(String action, JSONArray data,
 			CallbackContext callbackContext) throws JSONException {
+		this.callbackContext = callbackContext;
 
-		if (action.equals(ACTION)) {
-
-			String base64 = data.optString(0);
-			if (base64.equals("")) // isEmpty() requires API level 9
-				callbackContext.error("Missing base64 string");
-
-			// Create the bitmap from the base64 string
-			Log.d("Canvas2ImagePlugin", base64);
-			byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
-			Bitmap bmp = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-			if (bmp == null) {
-				callbackContext.error("The image could not be decoded");
-			} else {
-
-				// Save the image
-				File imageFile = savePhoto(bmp);
-				if (imageFile == null)
-					callbackContext.error("Error while saving image");
-
-				// Update image gallery
-				scanPhoto(imageFile);
-
-				callbackContext.success(imageFile.toString());
-			}
-
-			return true;
-		} else {
+		if (!action.equals(ACTION)) {
 			return false;
+		}
+
+		// First see if we can create the image
+		String base64 = data.optString(0);
+		if (base64.isEmpty()) {
+			callbackContext.error("Missing base64 string");
+			return true;
+		}
+
+		byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
+		Bitmap bmp = BitmapFactory.decodeByteArray(decodedString, STORE_PHOTO_REQ_CODE, decodedString.length);
+		if (bmp == null) {
+			callbackContext.error("The image could not be decoded");
+			return true;
+		}
+
+		// Now we have the image, see if we can store the image on the filesystem..
+		if (!cordova.hasPermission(WRITE_EXTERNAL_STORAGE)) {
+			this.bitmapImage = bmp;
+			cordova.requestPermission(this, 0, WRITE_EXTERNAL_STORAGE);
+			return true;
+		}
+
+		return storePhotoInGallery(bmp, callbackContext);
+	}
+
+	private boolean storePhotoInGallery(Bitmap image, CallbackContext callbackContext)
+	{
+		try {
+			File imageFile = storePhotoOnStorage(image);
+			sendImageGalleryUpdateRequest(imageFile);
+			callbackContext.success(imageFile.toString());
+			return true;
+
+		} catch (NullPointerException e) {
+			callbackContext.error("Error while saving image");
+		}
+
+		return false;
+	}
+
+	public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException
+	{
+		for (int r:grantResults) {
+			if (r == PackageManager.PERMISSION_DENIED) {
+				this.callbackContext.error("Permission denied");
+			}
+		}
+
+		switch(requestCode) {
+			case STORE_PHOTO_REQ_CODE:
+				storePhotoInGallery(this.bitmapImage, this.callbackContext);
+				break;
 		}
 	}
 
-	private File savePhoto(Bitmap bmp) {
+	private File storePhotoOnStorage(Bitmap bmp) {
 		File retVal = null;
 
 		try {
@@ -99,7 +137,7 @@ public class imageToGallerySaverPlugin extends CordovaPlugin {
 				folder = Environment.getExternalStorageDirectory();
 			}
 
-			File imageFile = new File(folder, "c2i_" + date.toString() + ".png");
+			File imageFile = new File(folder, "c2i_" + date + ".png");
 
 			FileOutputStream out = new FileOutputStream(imageFile);
 			bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
@@ -108,7 +146,7 @@ public class imageToGallerySaverPlugin extends CordovaPlugin {
 
 			retVal = imageFile;
 		} catch (Exception e) {
-			Log.e("imageToGallerySaverPlugin", "An exception occured while saving image: "
+			Log.e("imageToGallerySaver", "An exception occured while saving image: "
 					+ e.toString());
 		}
 		return retVal;
@@ -116,7 +154,7 @@ public class imageToGallerySaverPlugin extends CordovaPlugin {
 
 	/* Invoke the system's media scanner to add your photo to the Media Provider's database,
 	 * making it available in the Android Gallery application and to other apps. */
-	private void scanPhoto(File imageFile)
+	private void sendImageGalleryUpdateRequest(File imageFile)
 	{
 		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
 	    Uri contentUri = Uri.fromFile(imageFile);
--- src/ios/imageToGallerySaverPlugin.m
+++ src/ios/imageToGallerySaverPlugin.m
@@ -13,45 +13,34 @@
 @implementation imageToGallerySaverPlugin
 @synthesize callbackId;
 
-//-(CDVPlugin*) initWithWebView:(UIWebView*)theWebView
-//{
-//    self = (Base64ImageSaverPlugin*)[super initWithWebView:theWebView];
-//    return self;
-//}
-
 - (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
 {
-    self.callbackId = command.callbackId;
-	NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
+  self.callbackId = command.callbackId;
+  NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options: 0];
 
-	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];
-	UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-
+  UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];
+  UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
 }
 
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
 {
-    // Was there an error?
-    if (error != NULL)
-    {
-        // Show error message...
-        NSLog(@"ERROR: %@",error);
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-		[self.webView stringByEvaluatingJavaScriptFromString:[result toErrorCallbackString: self.callbackId]];
-    }
-    else  // No errors
-    {
-        // Show message image successfully saved
-        NSLog(@"IMAGE SAVED!");
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
-		[self.webView stringByEvaluatingJavaScriptFromString:[result toSuccessCallbackString: self.callbackId]];
-    }
+  CDVPluginResult* result = [CDVPluginResult
+                         resultWithStatus: CDVCommandStatus_OK
+                         messageAsString:@"Image saved"];
+
+  // Was there an error?
+  if (error != NULL)
+  {
+    result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+  }
+
+  [self.commandDelegate sendPluginResult: result callbackId:self.callbackId];
 }
 
 - (void)dealloc
 {
-	[callbackId release];
-    [super dealloc];
+  [callbackId release];
+  [super dealloc];
 }
 
 
